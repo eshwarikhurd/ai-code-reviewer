6db1465122f9283d0566de3943b1f47fa2c39d44
Eshwari Khurd <khurde@uci.edu>
2026-04-12 22:42:04 -0700

    remove test scaffolding files

--- src/test_sample.py
+++
@@ -1,6 +0,0 @@
-def calculate_discount(price, user):
-    conn = get_db_connection()
-    query = "SELECT discount FROM users WHERE name = '" + user + "'"
-    result = conn.execute(query)
-    discount = result[0]
-    return price - discount
